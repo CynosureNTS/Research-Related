9b9f91ff17d093d16fcf138adc83fc5ce57c8845
Michael Tran <hao.tran02@gmail.com>
2025-01-14 14:54:48 -0500

    Progress on GoL exercise set 02iff

--- Lean/GlimpseOfLean/GlimpseOfLean/Exercises/02Iff.lean
+++ Lean/GlimpseOfLean/GlimpseOfLean/Exercises/02Iff.lean
@@ -35,7 +35,11 @@ prove one-by-one.
 -/
 
 example (a b : ℝ) (ha : 0 < a) (hb : 0 < b) : 0 < a^2 + b^2 := by {
-  sorry
+  apply add_pos
+  apply sq_pos_of_pos
+  exact ha
+  apply sq_pos_of_pos
+  exact hb
 }
 
 /-
@@ -60,7 +64,15 @@ example (a : ℝ) (ha : 0 < a) : 0 < (a^2)^2 := by {
 /- Now prove the same lemma as before using forwards reasoning. -/
 
 example (a b : ℝ) (ha : 0 < a) (hb : 0 < b) : 0 < a^2 + b^2 := by {
-  sorry
+  have h3 : 0 < a^2 := by
+    apply sq_pos_of_pos
+    exact ha
+  have h4 : 0 < b^2 := by
+    apply sq_pos_of_pos
+    exact hb
+  apply add_pos
+  exact h3
+  exact h4
 }
 
 
@@ -190,4 +202,3 @@ equivalences. You learned about tactics:
 * `have`
 * `constructor`
 -/
-
